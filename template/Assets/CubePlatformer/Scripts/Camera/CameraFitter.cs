54954065147f34b1dc159c7e953d13458cc500d8
Maryia Ondra <ya.ondra@yandex.by>
2021-04-19 22:41:13 +0300

    added Camera Fitter

---
+++ template/Assets/CubePlatformer/Scripts/Camera/CameraFitter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubePlatformer
+{
+    [ExecuteAlways]
+    public class CameraFitter : MonoBehaviour
+    {
+        [SerializeField]
+        float width;
+
+        Camera fitCamera;
+
+        void OnEnable()
+        {
+            fitCamera = GetComponent<Camera>();
+        }
+
+        void Update()
+        {
+            fitCamera.orthographicSize = (float)Screen.height / Screen.width * width;
+        }
+    }
+}
