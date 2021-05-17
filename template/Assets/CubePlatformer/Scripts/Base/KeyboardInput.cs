cad2b722958a7b4f320dc6a433332f7219d6cf9b
Maryia Ondra <ya.ondra@yandex.by>
2021-05-17 17:27:58 +0300

    fixed delay of running

--- template/Assets/CubePlatformer/Scripts/Base/KeyboardInput.cs
+++ template/Assets/CubePlatformer/Scripts/Base/KeyboardInput.cs
@@ -8,8 +8,8 @@ namespace CubePlatformer
     {
         void Update()
         {
-            VirtualInputManager.Instance.MoveVertical = Input.GetAxis("Vertical");
-            VirtualInputManager.Instance.MoveHorizontal = Input.GetAxis("Horizontal");
+            VirtualInputManager.Instance.MoveVertical = Input.GetAxisRaw("Vertical");
+            VirtualInputManager.Instance.MoveHorizontal = Input.GetAxisRaw("Horizontal");
 
             VirtualInputManager.Instance.Attack = Input.GetKeyDown(KeyCode.F) ? true : false;
             VirtualInputManager.Instance.Defend = Input.GetKey(KeyCode.C) ? true : false;
